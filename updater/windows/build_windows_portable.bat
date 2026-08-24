cddde5c2d1e9340bba6b2fade627d6b57c940cc2
dosordie <14365099+dosordie@users.noreply.github.com>
2026-08-24 17:17:47 +0200

    Bump Windows portable build to v0.1.3

--- updater/windows/build_windows_portable.bat
+++ updater/windows/build_windows_portable.bat
@@ -2,7 +2,7 @@
 setlocal EnableExtensions EnableDelayedExpansion
 cd /d "%~dp0\..\.."
 
-set "APP_VERSION=0.1.2"
+set "APP_VERSION=0.1.3"
 set "APP_NAME=FoxAir_Updater"
 set "OUT=dist\%APP_NAME%"
 set "CACHE=build\windows-cache"
